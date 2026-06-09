2934bfd3d25ecf70e044839a3338ea53f6cbed8b
obyed <obyed.oss@gmail.com>
2026-06-08 20:54:28 -0400

    log iin with env

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,18 +1,25 @@
 import { test, expect } from '@playwright/test';
-import { LoginPage } from '../pages/LoginPage';
 
 test('User can login successfully', async ({ page }) => {
-  const loginPage = new LoginPage(page);
+  await page.goto('https://www.saucedemo.com/');
 
-  await page.goto('/');
-
-  await loginPage.login(
-
-    
-    process.env.USERNAME!,
-    process.env.PASSWORD!
-  );
+await page.fill('#user-name', process.env.SAUCE_USERNAME!);
+await page.fill('#password', process.env.SAUCE_PASSWORD!);
+  await page.click('#login-button');
 
   await expect(page).toHaveURL(/inventory.html/);
   await expect(page.locator('.title')).toHaveText('Products');
 });
+
+// import { test, expect } from '@playwright/test';
+
+// test('User can login successfully', async ({ page }) => {
+//   await page.goto('https://www.saucedemo.com/');
+
+//   await page.fill('#user-name', 'standard_user');
+//   await page.fill('#password', 'secret_sauce');
+//   await page.click('#login-button');
+
+//   await expect(page).toHaveURL(/inventory.html/);
+//   await expect(page.locator('.title')).toHaveText('Products');
+// });
